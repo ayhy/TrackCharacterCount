5ac491684f7144a8710043ed051b5b6a194adcd6
ayhy <7615889+ayhy@users.noreply.github.com>
2019-10-05 04:21:37 +0900

    separate-track flag
    added "separate_track" flag to cover multiple files but one long entry e.g. book chapters

--- tracker.py
+++ tracker.py
@@ -4,13 +4,26 @@
 import sys
 import re
 from datetime import datetime
-from collections import Counter
+from functools import reduce
 import math
 
+def getCurrentCharacterCount(targetFileName):
+    totalchar=0
+    totalmainchar=0
+    with open(targetFileName, 'r', encoding='utf-8') as readfile:
+        text=readfile.read().strip("/n")
+        main_char = re.sub("(<!--.*?-->)", "", text, flags=re.DOTALL) #remove  <!--comment-->
+        main_char = re.sub("(/\*.*?\*/)", "", main_char, flags=re.DOTALL) #remove  /* comment */
+        totalchar= len(text.strip("#"))
+        totalmainchar=len((main_char.strip("#"))) # stripped markdown syntax
+    return {"total":totalchar,"main":totalmainchar}
+
+    return 
 def updatelog(fullfilename):
     targetFiles=[]
     targetChars=[]
     targetDates=[]
+    track_separate=False
     lastLine=[]
 
     now = datetime.now()
@@ -25,12 +38,14 @@ def updatelog(fullfilename):
                 is_in_ini=False
                 break
             if is_in_ini:
+                if line.startswith("track_separate"):
+                    tracks_separate = (line == "True")
                 if line.startswith("fullfilepath"):
                     targetFiles.append(line.split("\t")[1].strip("\n").replace("\\", "/") )
                 if line.startswith("deadline"):
-                    date=line.split("\t")[1].strip("\n")
-                    date=datetime.strptime(date,'%Y/%m/%d')
-                    targetDates.append(date)
+                    deadline=line.split("\t")[1].strip("\n")
+                    deadline=datetime.strptime(deadline,'%Y/%m/%d')
+                    targetDates.append(deadline)
                 if line.startswith("totalchar"):
                     char=line.split("\t")[1].strip("\n")
                     targetChars.append(int(char))
@@ -38,37 +53,51 @@ def updatelog(fullfilename):
         lastLine = (list(infile)[-1])
         print(lastLine)
 
+    todaysprogress = now.strftime('%Y/%m/%d')
 
     targetNum=len(targetFiles)
+    countlist=[]
     previousTotalChars=[0]*targetNum
     previousMainChars=[0]*targetNum
+    remaining = [date - today for date in targetDates]
+
+    scanrows=targetNum
+    if track_separate==False:
+        scanrows=1
     print(str(targetNum))
     if not lastLine.startswith("//"):
         print("get comparison data")
-    #format:0ì˙ït 1ëSï∂ 2ñ{ï∂ 3ñ{ï∂+ 4ï‚çe+ 5ë¨ìx n6empty 7ëSï∂ 8ñ{ï∂ 9ñ{ï∂+ 10ï‚çe+ 11ë¨ìx 12empty .....
+    #format:0Êó•‰ªò 1ÂÖ®Êñá 2Êú¨Êñá 3Êú¨Êñá+ 4Ë£úÁ®ø+ 5ÈÄüÂ∫¶ 6-- 7ÂÖ®Êñá 8Êú¨Êñá 9Êú¨Êñá+ 10Ë£úÁ®ø+ 11ÈÄüÂ∫¶ 12-- .....
         lastLine=lastLine.split("\t")
         print(lastLine)
-        for i in range(targetNum):
+        for i in range(scanrows):
             previousTotalChars[i]=int(lastLine[i*6+1])
             previousMainChars[i]=int(lastLine[i*6+2])
-    todaysprogress = now.strftime('%Y/%m/%d')
     for i, targetFile in enumerate(targetFiles):
         with open(targetFile, 'r', encoding='utf-8') as readfile:
-            text=readfile.read().strip("/n")
-            main_char = re.sub("(<!--.*?-->)", "", text, flags=re.DOTALL) #remove  <!--comment-->
-            main_char = re.sub("(/\*.*?\*/)", "", main_char, flags=re.DOTALL) #remove  /* comment */
-            totalchar= len(text.strip("#"))
-            totalmainchar=len((main_char.strip("#"))) # stripped markdown syntax
+            counts = getCurrentCharacterCount(targetFile)
+        counts["deltaMain"]=counts["main"] - previousMainChars[i]
+        counts["deltaComment"]=counts["total"] - previousTotalChars[i] - counts["main"]
+        counts["deltaDate"]=targetDates[i] - today
+        counts["charRate"]=math.ceil( (targetChars[i]- counts["main"])/remaining[i].days) 
+        countlist.append(counts)
 
-        addedmainchar = totalmainchar - previousMainChars[i]
-        addedcommentchar = totalchar - previousTotalChars[i] - addedmainchar
-        remaining = targetDates[i] - today
-        advisedrate = math.ceil( (targetChars[i]- totalmainchar)/remaining.days) 
-        progressparFile = "\t" + str(totalchar) + "\t" + str(totalmainchar) + "\t" \
-                                    + str(addedmainchar) + "\t" + str(addedcommentchar) + "\t" \
-                                    + str(advisedrate) + "\t"
-        todaysprogress +=progressparFile
-    todaysprogress +="\n"
+    if track_separate:
+        for i, targetFile in enumerate(targetFiles):
+            progressparFile = "\t" + str(countlist[i]["total"]) + "\t" + str(countlist[i]["main"]) + "\t" \
+                                   + str(countlist[i]["deltaMain"]) + "\t" + str(countlist[i]["deltaComment"]) + "\t" \
+                                   + str(advisedrates[i]) + "\t"
+            todaysprogress +=progressparFile
+        todaysprogress +="\n"
+    else:
+        print(countlist)
+        counted = reduce(lambda x, y: dict((k, v + y[k]) for k, v in x.items()), countlist)
+        advisedrate =math.ceil( (sum(targetChars) - counted["main"]) / min(remaining).days)
+        progressMerged = "\t" + str(counted["total"]) + "\t" + str(counted["main"]) + "\t" \
+                              + str(counted["deltaMain"]) + "\t" + str(counted["deltaComment"]) + "\t" \
+                              + str(advisedrate) + "\t"
+        todaysprogress += progressMerged + "\n"
+
     with open(fullfilename, 'a', encoding='utf-8') as outfile:
         outfile.write(todaysprogress)
         print(todaysprogress)
